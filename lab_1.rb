e950aebee8ba98fee87bc0d10903dc0720d3925b
sara yasser <sarayasser2008@hotmail.com>
2020-04-27 13:25:48 +0200

    Revert "bulk added"
    This reverts commit b4d2e0082be1de660b78d0ab4c39a5369e2d2236.

--- lab_1.rb
+++ lab_1.rb
@@ -27,33 +27,22 @@ class ComplexNumbers
         self
         end		 	
 	
-	def bulk_mul()
-			new_a=@a[0]*@a[1] - @b[0]*@b[1]
-			new_b = @b[0]*@a[1] + @a[0]*@b[1]
-			newa = new_a*@a[2] - new_b*@b[2]
-			newb = new_b*@a[2] + new_a*@b[2]
-			@a=newa
-			@b=newb
-	self
-	end
 		
 	def ComplexNumbers.count
 		@@count
 	end
 
 	def display_results()
-		puts "result #@a + #@b i"
+		puts "result #@a + #@b"
 	end
 end
 
 multiply=ComplexNumbers.new(3,2)*ComplexNumbers.new(1,7)
 addition = ComplexNumbers.new(3,2)+ComplexNumbers.new(1,7)
 bulkadd = ComplexNumbers.new([2,3,1],[4,2,7]).bulk_add()
-bulkmul = ComplexNumbers.new([3,1,3],[2,7,5]).bulk_mul()
 
 puts addition.display_results()
 puts multiply.display_results()
 puts bulkadd.display_results()
-puts bulkmul.display_results()
 
 puts ComplexNumbers.count
