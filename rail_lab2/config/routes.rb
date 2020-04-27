f7fff4e0da5937811b89d47617d890993051974b
sara yasser <sarayasser2008@hotmail.com>
2020-04-27 13:43:18 +0200

    removed extra folder

--- rail_lab2/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :articles  do
-    resources :comments
-  end
-  resources :users
-  resources :sessions
-  get 'welcome/index' 
-  root 'users#index'
-end
